b47f929ce395a3a2a95dc9e7d5ccdd70bfdb3cff
wattipong lalae <wattipong@seiko.co.th>
2020-03-09 23:03:22 +0700

    Commit

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const os = require('os');
 const helmet = require('helmet');
+
+const http = require('http');
 //Import Routes
 const usersRouter = require('./routes/users');
 
@@ -33,23 +35,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Call Routes
 app.use('/users', usersRouter);
 
+//Fix Port Run Internal
+http.createServer(app).listen(8080)
 
-// catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//   next(createError(404));
-// });
+//Fix Runing External
+//app.listen(process.env.PORT || process.env.PORT_SERVER, () => console.log('express server listenting on - http://' + os.hostname() + ':' + process.env.PORT));
 
-// // error handler
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
-app.listen(process.env.PORT || process.env.PORT_SERVER, () => console.log('express server listenting on - http://' + os.hostname() + ':' + process.env.PORT));
 
 
 module.exports = app;
